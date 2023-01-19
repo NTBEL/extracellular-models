56068a6ffa04748cfe857cb986901cc3bf091cd8
blakeaw <blakeaw1102@gmail.com>
2023-01-19 17:14:41 -0600

    Updated version number in the setup.py script.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="extmodels",
-    version="0.1.0",
+    version="0.2.0",
     python_requires='>=3.9',
     install_requires=['numpy', 'scipy', 'matplotlib', 'neuron'],
     author="Blake A. Wilson",
